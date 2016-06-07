745d134fd08b67639c29209602b7cabefb8c842b
Benjamin <benjaminlivingston@gmail.com>
2016-06-07 13:06:07 -0700

    Add project files.

---
+++ menu.js
@@ -0,0 +1,17 @@
+﻿$(function(){
+
+    $("ul.dropdown li").mouseover(function(){
+    
+        $(this).addClass("hover");
+        $('ul:first',this).css('visibility', 'visible');
+    
+    }, mouseout(function(){
+    
+        $(this).removeClass("hover");
+        $('ul:first',this).css('visibility', 'hidden');
+    
+    });)
+    
+    $("ul.dropdown li ul li:has(ul)").find("a:first");//.append(" &raquo; ");
+
+});
