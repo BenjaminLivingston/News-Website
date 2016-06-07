c7592df377d4c0378a336ef4b6149212c9f55b19
Benjamin <benjaminlivingston@gmail.com>
2016-06-07 14:19:35 -0700

    Before trying IDs.

--- menu.js
+++ menu.js
@@ -1,17 +1,7 @@
-﻿$(function(){
+﻿$(document).ready(function () {
 
-    $("ul.dropdown li").mouseover(function(){
-    
-        $(this).addClass("hover");
-        $('ul:first',this).css('visibility', 'visible');
-    
-    }, mouseout(function(){
-    
-        $(this).removeClass("hover");
-        $('ul:first',this).css('visibility', 'hidden');
-    
-    });)
-    
-    $("ul.dropdown li ul li:has(ul)").find("a:first");//.append(" &raquo; ");
+    $("ul.dropdown").mouseover(function () {
+        $("ul.submenu").children().hide(1000);
+    });
 
 });
